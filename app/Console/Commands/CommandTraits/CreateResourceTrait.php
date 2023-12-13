77620366b8f9c1dab7fe0a385ac865e13eeaf807
Almir Mustafic <almir.mustafic.tz@gmail.com>
2023-12-14 00:01:05 +0100

    cache-clearing-commands-added

--- app/Console/Commands/CommandTraits/CreateResourceTrait.php
+++ app/Console/Commands/CommandTraits/CreateResourceTrait.php
@@ -10,10 +10,13 @@ trait CreateResourceTrait
     {
         Artisan::call('make:model', ['name' => $modelName]);
         $this->info("Model created: $modelName");
+
         Artisan::call('make:factory', ['name' => $modelName . 'Factory']);
         $this->info("Factory created: $modelName" . 'Factory');
+
         Artisan::call('make:controller', ['name' => 'Api/' . $modelName . 'Controller']);
         $this->info("Controller created: $modelName" . 'Controller');
+
         Artisan::call('make:migration', ['name' => 'create_' . strtolower($modelName) . 's_table']);
         $this->info("Migration created: create_" . strtolower($modelName) . 's_table');
     }
--- app/Console/Commands/MakeResourceWithRepositoryCommand.php
+++ app/Console/Commands/MakeResourceWithRepositoryCommand.php
@@ -4,6 +4,9 @@ namespace App\Console\Commands;
 
 use App\Console\Commands\CommandTraits\CreateDataAccessLayerFoldersTrait;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class MakeResourceWithRepositoryCommand extends Command
 {
@@ -18,6 +21,7 @@ class MakeResourceWithRepositoryCommand extends Command
     use CommandTraits\CreateRepositoryServiceTrait;
     use CommandTraits\CreateRoutesTrait;
 
+    // TODO: Add option to create a controller, routes, and register the interface in the service provider
     protected $signature = 'make:resource {name : The name of the Eloquent model} {--repository : Include a repository}';
     protected $description = 'Generate an Eloquent model with an optional repository';
 
@@ -48,5 +52,15 @@ class MakeResourceWithRepositoryCommand extends Command
         }
 
         $this->info("Generation complete!");
+
+        Artisan::call('optimize');
+        $this->info('Cache cleared');
+
+        $process = new Process(['composer', 'dump-autoload']);
+        $process->run();
+
+        $process->isSuccessful()
+            ? $this->info('Composer dump-auto-loaded!')
+            : throw new ProcessFailedException($process);
     }
 }
